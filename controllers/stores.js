339a784bbd6a395f5e06e2200f34a817e86c55f9
Arif Rakhman <kamucantiksekali@gmail.com>
2020-08-17 11:23:33 +0700

    adding add store

--- controllers/stores.js
+++ controllers/stores.js
@@ -1,3 +1,29 @@
+const Store = require('../models/Store');
+
 exports.getStores = async (req, res, next) => {
-    res.send('hello');
+    try{
+        const store = await Store.find();
+
+        return res.json({
+            success: true,
+            count: store.length,
+            data: store
+        });
+    }catch (e) {
+        console.log(e);
+        return res.status(500).json(e);
+    }
+}
+
+exports.addStores = async (req, res, next) => {
+    try{
+        const store = await Store.create(req.body);
+        return res.json({
+            success: true,
+            data: store
+        });
+    }catch (e) {
+        console.log(e);
+        return res.status(500).json(e);
+    }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
 const router = require('express').Router();
-const {getStores} = require('../controllers/stores');
-router.get('/stores', getStores);
+const {getStores, addStores} = require('../controllers/stores');
+router.route('/stores').get(getStores).post(addStores);
 module.exports = router;
